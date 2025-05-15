c405976beb0a3efbca0b60efc01b98678f071d1b
yongtaoyu <105630510+yongtaoyu@users.noreply.github.com>
2025-05-15 22:17:53 +0800

    Add files via upload

---
+++ site01/11.js
@@ -0,0 +1,8 @@
+function sayHelllo(name) {
+  const greeting = `你好，${name}！`;
+  return greeting;
+}
+
+const helloChris = sayHelllo("Chris");
+
+console.log(helloChris);
---
+++ site01/scripts/main.js
@@ -0,0 +1,34 @@
+const myImage = document.querySelector("img");
+
+myImage.onclick = () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/SVE Logo.png") {
+    myImage.setAttribute("src", "images/wallhaven-2ye18x.jpg");
+  } else {
+    myImage.setAttribute("src", "images/SVE Logo.png");
+  }
+};
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+  const myName = prompt("Please enter your name.");
+  if (!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem("name", myName);
+    myHeading.textContent = `Mozilla is cool, ${myName}`;
+  }
+}
+
+if (!localStorage.getItem("name")) {
+  setUserName();
+} else {
+  const storedName = localStorage.getItem("name");
+  myHeading.textContent = `Mozilla is cool, ${storedName}`;
+}
+
+myButton.onclick = function () {
+  setUserName();
+};
