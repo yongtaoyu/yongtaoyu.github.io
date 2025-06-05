82e082f0ea4996c98aca46039fcd047ec2b6b613
yuyt <2602081704@qq.com>
2025-06-05 17:21:44 +0800

    layout-渲染一级导航列表

--- src/main.js
+++ src/main.js
@@ -6,12 +6,6 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
-//测试api
-import { getCategoryAPI } from '@/apis/testAPI'
-getCategoryAPI().then(res => {
-  console.log(res);
-})
-
 
 const app = createApp(App)
 
