941519cc3938a0c04a22d5e44219b678f4a19069
yuyt <2602081704@qq.com>
2025-06-05 18:31:52 +0800

    Pinia优化重复请求

--- src/views/Layout/components/LayoutHeader.vue
+++ src/views/Layout/components/LayoutHeader.vue
@@ -1,17 +1,10 @@
 <script setup>
 import { getCategoryAPI } from '@/apis/layoutAPI';
 import { onMounted, ref } from 'vue';
+import { useCategoryStore } from '@/stores/category';
 
+const category = useCategoryStore()
 
-const categoryList = ref([])
-const getCategory = async () => {
-  const res = await getCategoryAPI()
-  categoryList.value = res.result
-}
-
-onMounted(() => {
-  getCategory()
-})
 </script>
 
 <template>
@@ -21,7 +14,7 @@ onMounted(() => {
         <RouterLink to="/">小兔鲜</RouterLink>
       </h1>
       <ul class="app-header-nav">
-        <li class="home" v-for="item in categoryList" :key="item.id">
+        <li class="home" v-for="item in category.categoryList" :key="item.id">
           <RouterLink to="/">{{ item.name }}</RouterLink>
         </li>
 
