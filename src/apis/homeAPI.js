3ed197c357babe8e0c762c1f65e70017d190aaf4
yuyt <2602081704@qq.com>
2025-06-06 22:06:31 +0800

    新鲜好物人气推荐实现

--- src/apis/homeAPI.js
+++ src/apis/homeAPI.js
@@ -1,7 +1,31 @@
 import http from "@/utils/http";
 
+
+//获取轮播图
 export function getBannerAPI() {
   return http({
     url: '/home/banner'
+  })
+}
+
+/**
+ * @description: 获取新鲜好物
+ * @param {*}
+ * @return {*}
+ */
+export const findNewAPI = () => {
+  return http({
+    url: '/home/new'
+  })
+}
+
+/**
+ * @description: 获取人气推荐
+ * @param {*}
+ * @return {*}
+ */
+export const getHotAPI = () => {
+  return http({
+    url: '/home/hot'
   })
 }
--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -4,7 +4,6 @@ import HomeBanner from './components/HomeBanner.vue'
 import HomeNew from './components/HomeNew.vue'
 import HomeHot from './components/HomeHot.vue'
 import HomeProduct from './components/HomeProduct.vue'
-import HomePanel from './components/HomePanel.vue'
 </script>
 
 <template>
@@ -15,11 +14,4 @@ import HomePanel from './components/HomePanel.vue'
   <HomeNew />
   <HomeHot />
   <HomeProduct />
-
-  <HomePanel title="新鲜好物" sub-title="新鲜好物 好多商品">
-    <div>我是新鲜好物插槽内容</div>
-  </HomePanel>
-  <HomePanel title="人气推荐" sub-title="人气推荐 好多商品">
-    <div>我是人气推荐插槽内容</div>
-  </HomePanel>
 </template>
